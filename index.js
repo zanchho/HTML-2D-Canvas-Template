91cb68c2444e640f54e142086fb35b30f3416a10
zanchho <zanchho391@gmail.com>
2024-05-31 07:16:26 +0200

    inplemented content to GridItem for drawing

--- index.js
+++ index.js
@@ -15,15 +15,26 @@ function drawRect(x, y, w, h, color) {
   ctx.fillRect(x, y, w, h)
 }
 
-function drawCircle(x, y, radius, startAngle, endAngle, color) {
+function drawCircle(x, y, radius, startAngle, endAngle, color, filled) {
+  if (!filled) filled = false
   ctx.strokeStyle = color
   ctx.beginPath()
   ctx.arc(x, y, radius, startAngle, endAngle)
-  ctx.stroke()
+  if (filled) {
+    ctx.fillStyle = color
+    ctx.fill()
+  } else {
+    ctx.stroke()
+  }
 }
 
-function drawText(text, x, y, font) {
-  if (font) ctx.font = font
+function drawText(text, x, y, fontSize, fontFamily, color) {
+  if (fontSize && fontFamily) {
+    ctx.font = `${fontSize}px ${fontFamily}`
+  }
+  if (color) {
+    ctx.fillStyle = color
+  }
   ctx.fillText(text, x, y)
 }
 
@@ -36,6 +47,53 @@ class GridItem {
     this.row = row
     this.col = col
     this.color = color
+
+    this.content
+    this.contentType
+    this.contentWaiting = false
+    this.draw = () => {
+      if (!this.contentType) return
+
+      switch (this.contentType) {
+        case "img":
+          ctx.drawImage(this.content, this.x, this.y, this.width, this.height)
+          break
+        case "rect":
+          drawRect(this.x, this.y, this.width, this.height, this.content)
+          break
+        case "circle":
+          let hwMin = this.width > this.height ? this.height : this.width
+          let middle = {
+            x: Math.floor(this.x + this.width / 2),
+            y: Math.floor(this.y + this.height / 2),
+          }
+          drawCircle(
+            middle.x,
+            middle.y,
+            Math.floor(hwMin / 2),
+            0,
+            2 * Math.PI,
+            this.color
+          )
+          break
+        case "text":
+          //set values for ctx
+          ctx.font = "100% Arial"
+          ctx.fillStyle = "red"
+
+          let metrics = ctx.measureText(this.content)
+          //center and move back to align text in the middle of this
+          let adjustedX = this.x + this.width / 2 - metrics.width / 2
+          let adjustedY = this.y + this.height / 2
+          drawText(this.content, adjustedX, adjustedY, "100%", "Arial", "red")
+          break
+        default:
+          throw new Error(`Unknown ContentType in gridItem: ${this}`)
+      }
+    }
+    this.isContentReady = () => {
+      return !this.contentWaiting
+    }
   }
 }
 function createGrid(rows, columns, color) {
@@ -60,47 +118,104 @@ function createGrid(rows, columns, color) {
   return grid
 }
 function drawGrid(gridArray) {
-  gridArray.forEach(grid => {
+  gridArray.forEach(gridItem => {
+    if (gridItem.isContentReady) gridItem.draw()
     ctx.beginPath()
-    ctx.rect(grid.x, grid.y, grid.width, grid.height)
-    ctx.strokeStyle = grid.color
+    ctx.rect(gridItem.x, gridItem.y, gridItem.width, gridItem.height)
+    ctx.strokeStyle = "#FFF"
     ctx.lineWidth = 1
     ctx.stroke()
   })
 }
 
-function loadRandomImageAndDraw(gridToDraw) {
+async function loadRandomImage(gridToDraw) {
+  gridToDraw.contentWaiting = true
   const img = new Image()
   // https://picsum.photos/width/height
-  img.src = `https://picsum.photos/1920/1080.jpg`
-  img.onload = function () {
-    console.log("Image Loaded")
-    ctx.drawImage(
-      img,
-      gridToDraw.x,
-      gridToDraw.y,
-      gridToDraw.width,
-      gridToDraw.height
-    )
+  img.src = `https://picsum.photos/1920/1080?random=${Math.random() * 1000}.jpg`
+  img.onload = () => {
+    console.log("image loaded")
+    gridToDraw.contentType = "img"
+    gridToDraw.content = img
+    gridToDraw.contentWaiting = false
+  }
+  img.onerror = () => {
+    console.error("Failed to load image:", img.src)
+    gridToDraw.contentType = "error"
+    gridToDraw.content = null
+    gridToDraw.contentWaiting = false
   }
   console.log(`waiting for ${img} to load`)
 }
 
 function getGridItem(atRow, atCol) {
   return gridArray.find(({ col, row }) => {
-    return col === atCol - 1 && row === atRow - 1
+    return col === atCol && row === atRow
   })
 }
-//testing functions:
-drawRect(190, 190, 70, 70, "aqua")
-drawCircle(200, 200, 50, 0, Math.PI * 2, "red")
-drawText("Hello World", 250, 300, "24px Arial")
 
+//usage example:
 const gridArray = createGrid(5, 5, "lightgray")
 
-const gridToDraw = getGridItem(2, 2)
-const gridToDraw2 = getGridItem(2, 3)
-if (gridToDraw) loadRandomImageAndDraw(gridToDraw)
-if (gridToDraw2) loadRandomImageAndDraw(gridToDraw2)
+let fps, lastTimestamp
+function handleFPS(timestamp) {
+  const deltaTime = timestamp - lastTimestamp
+  lastTimestamp = timestamp
 
-drawGrid(gridArray)
+  fps = 1000 / deltaTime
+}
+
+const drawAll = () => {
+  //ungridded -mostlikely useless to me
+  drawRect(190, 190, 70, 70, "aqua")
+  drawCircle(200, 200, 50, 0, Math.PI * 2, "red")
+  drawText("Hello World", 620, 250, "100%", "Arial", "red")
+
+  drawGrid(gridArray)
+}
+const reset = () => {
+  drawRect(0, 0, canvas.width, canvas.height, "#000")
+}
+const update = () => {
+  drawAll()
+
+  //draw fps
+  if (fps) drawText(fps.toFixed(0), 10, 20, "100%", "Arial", "#FFF")
+
+  const timestamp = performance.now()
+  requestAnimationFrame(reset)
+  requestAnimationFrame(handleFPS)
+  requestAnimationFrame(update)
+}
+update()
+
+//#region  test images
+const imageGridItem = getGridItem(4, 4)
+// Gen random delay
+const delay = Math.floor(Math.random() * 11) + 3
+
+const loadImage = async () => {
+  await loadRandomImage(imageGridItem)
+
+  setTimeout(() => {
+    loadImage()
+  }, delay * 1000) // ms to s
+}
+loadImage()
+
+//#endregion
+//#region test Rect
+const rectGridItem = getGridItem(0, 3)
+rectGridItem.content = "#123"
+rectGridItem.contentType = "rect"
+//#endregion
+//#region test Circle
+const circleGridItem = getGridItem(1, 3)
+circleGridItem.content = "#123"
+circleGridItem.contentType = "circle"
+//#endregion
+//#region test Text
+const textGridItem = getGridItem(1, 2)
+textGridItem.content = "Hello My Cutie Grid"
+textGridItem.contentType = "text"
+//#endregion
